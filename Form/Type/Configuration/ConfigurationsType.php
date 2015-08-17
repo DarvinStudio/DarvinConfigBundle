f0688abf08d6bdb2b55b060f53d2b2a9c9484d07
igor <igor.sv.n@gmail.com>
2015-08-17 12:38:47 +0300

    Move configuration form types from AdminBundle to ConfigBundle.

---
+++ Form/Type/Configuration/ConfigurationsType.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 14.08.15
+ * Time: 15:23
+ */
+
+namespace Darvin\ConfigBundle\Form\Type\Configuration;
+
+use Darvin\ConfigBundle\Configuration\ConfigurationPool;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Configurations form type
+ */
+class ConfigurationsType extends AbstractType
+{
+    /**
+     * @var \Darvin\ConfigBundle\Configuration\ConfigurationPool
+     */
+    private $configurationPool;
+
+    /**
+     * @param \Darvin\ConfigBundle\Configuration\ConfigurationPool $configurationPool Configuration pool
+     */
+    public function __construct(ConfigurationPool $configurationPool)
+    {
+        $this->configurationPool = $configurationPool;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        foreach ($this->configurationPool->getAll() as $configuration) {
+            $builder->add($configuration->getName(), new ConfigurationType($configuration), array(
+                'label' => sprintf('configuration.%s.title', $configuration->getName()),
+            ));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'cascade_validation' => true,
+            'data_class'         => get_class($this->configurationPool),
+            'intention'          => md5(__FILE__),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'darvin_config_configurations';
+    }
+}
