a2bec51b1c82407b16e705a3c2a322e8d9f4e78b
igor <igor.sv.n@gmail.com>
2015-11-18 16:54:08 +0300

    Replace form type name with FQCN in getParent() methods.

--- Form/Type/Configuration/ConfigurationType.php
+++ Form/Type/Configuration/ConfigurationType.php
@@ -28,7 +28,7 @@ class ConfigurationType extends AbstractType
     private static $fieldTypes = array(
         ParameterModel::TYPE_ARRAY   => 'Symfony\\Component\\Form\\Extension\\Core\\Type\\CollectionType',
         ParameterModel::TYPE_BOOL    => 'Symfony\\Component\\Form\\Extension\\Core\\Type\\CheckboxType',
-        ParameterModel::TYPE_ENTITY  => 'Symfony\\Component\\Form\\Extension\\Core\\Type\\EntityType',
+        ParameterModel::TYPE_ENTITY  => 'Symfony\\Bridge\\Doctrine\\Form\\Type\\EntityType',
         ParameterModel::TYPE_FLOAT   => 'Symfony\\Component\\Form\\Extension\\Core\\Type\\NumberType',
         ParameterModel::TYPE_INTEGER => 'Symfony\\Component\\Form\\Extension\\Core\\Type\\IntegerType',
     );
