264b8c7d727f51b47f87124396a59db08b9c5475
Igor Nikolaev <igor.sv.n@gmail.com>
2019-01-17 09:22:30 +0300

    .yml => .yaml

--- DependencyInjection/DarvinConfigExtension.php
+++ DependencyInjection/DarvinConfigExtension.php
@@ -31,7 +31,7 @@ class DarvinConfigExtension extends Extension
 //        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('configuration.yml');
-        $loader->load('parameter.yml');
+        $loader->load('configuration.yaml');
+        $loader->load('parameter.yaml');
     }
 }
